09b403f1a6937c135e2dacf6c1c79a49ed4d5323
Mahmood Garibov <mahmood.garibov@gmail.com>
2022-07-28 21:26:52 +0400

    Blog model created

---
+++ blog-manageng/blog-manageng/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace blog_manageng
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                
+
+        }
+
+    }
+
+    class Blog
+    {
+        public Blog(string title, string content, BlogStatus status, params string[] tags)
+        {
+            Title = title;
+            Content = content;
+            Status = status;
+            Tags = tags;
+        }
+
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public BlogStatus Status { get; set; }
+        public string[] Tags { get; set; }
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+    }
+
+    enum BlogStatus
+    {
+        Created,
+        Approved,
+        Rejected
+    }
+
+
+
+}
